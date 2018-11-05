8e59fab4a29a202e696443ce4cf7f86263b6a3db
mamta-dhaka <ug201211019@iitj.ac.in>
2018-11-05 14:15:48 +0530

    ad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,13 @@ import { MomentModule } from 'angular2-moment';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ConnectionService } from './services/connection.service';
 import { DashboardModule } from './dashboard';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {DriverComponent} from './driver/driver.component';
+import { CustomerComponent } from './customer/customer.component';
 
 import { CustomerModule } from './customer';
 import {DriverModule} from './driver';
@@ -21,7 +25,16 @@ import {DriverModule} from './driver';
     CustomerModule,
     DashboardModule,
     DriverModule,
-    MomentModule
+    MomentModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/customer', pathMatch: 'full'},
+      { path: 'customer', component: CustomerComponent, loadChildren: (): any => CustomerModule, },
+      { path: 'dashboard', component: DashboardComponent, loadChildren: (): any => DashboardModule, },
+      {
+        path: 'driver',
+        component: DriverComponent, loadChildren: (): any => DriverModule,
+      },
+    ]),
   ],
   providers: [ ConnectionService ],
   bootstrap: [AppComponent]
